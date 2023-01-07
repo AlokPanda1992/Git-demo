01ef464525f71ccfcf4a80aa75acdc369bc4c5dc
krisha <alokpanda63@gmail.com>
2023-01-07 15:11:07 +0530

    add string pset-6

---
+++ pset-6.py
@@ -0,0 +1,9 @@
+"""Pset-6"""
+# input Fist name 
+#input last name 
+#print full name
+
+
+fname = input("Input your First Name : ")
+lname = input("Input your Last Name : ")
+print ("Hello  " + fname + " " + lname)
