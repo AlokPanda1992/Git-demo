8ffd4af21cbfeb43c22516d68b76016ef64f4cc8
AlokPanda1992 <120553275+AlokPanda1992@users.noreply.github.com>
2023-01-07 12:41:05 +0530

    Add files via upload

---
+++ psets-9.py
@@ -0,0 +1,8 @@
+"""psets-9"""
+# Replace "a" with "b"
+# entr a string contaning the  name  a
+
+
+str_in = input ("the house is a big\n")
+new_str = str_in.replace('a','b')
+print("the new string after replacement is",new_str)
