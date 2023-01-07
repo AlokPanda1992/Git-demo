2f069ca26ac8dcfd605f9b9fa5c7fd42af8a5844
AlokPanda1992 <120553275+AlokPanda1992@users.noreply.github.com>
2023-01-07 12:06:52 +0530

    Add files via upload

---
+++ psets-2.py
@@ -0,0 +1,7 @@
+""""Pset-2"""
+# step 1 :Read the input
+# step 2  :Print the Name
+
+Name = input ("Enter Your Name  Hear\n")
+Name = print("Welcome",Name)
+
--- readme.md
+++
@@ -1,1 +0,0 @@
-This is a repository containg psets and salution for the same
